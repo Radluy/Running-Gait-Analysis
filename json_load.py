33b926e8cf63386575ea8f27fa214f0578928195
Radoslav Elias <radoslave0@gmail.com>
2021-03-08 12:54:09 +0100

    load only keypoints

--- json_load.py
+++ json_load.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import os
+import pprint
 
 # data = [frame1, frame2, ..., frame n]
 # frame = {
@@ -14,19 +15,25 @@ import os
 
 
 def load_json(json_directory_path: str): 
-""" load json files from a directory and return as an array of dictionaries"""
+    """ load json files from a directory and return as an array of dictionaries"""
+    data = []
     files = os.listdir(json_directory_path)
     for file in files:
         path = os.path.join(json_directory_path, file)
         with open(path) as json_file:
-            data = json.load(json_file)
-            
-            return data
+            json_struct = json.load(json_file)
+            try:
+                keypoints = json_struct["people"][0]["pose_keypoints_2d"]
+                data.append(keypoints)
+            except:
+                pass
+    return data
 
 
 if __name__ == "__main__": 
     #load json data from arg1 path
     directory = sys.argv[1]
-    load_json(directory)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(load_json(directory))
 
     
