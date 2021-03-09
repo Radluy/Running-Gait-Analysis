4679ce48de29d0e3e7188a4cc2da199d040a2826
Radoslav Elias <radoslave0@gmail.com>
2021-03-09 14:58:46 +0100

    added frame Id

--- json_load.py
+++ json_load.py
@@ -31,7 +31,9 @@ def load_json(json_directory_path: str) -> list:
     """ load json files from a directory and return as an array of dictionaries"""
     data = []
     files = os.listdir(json_directory_path)
+    counter = -1
     for file in files:
+        counter += 1
         path = os.path.join(json_directory_path, file)
         with open(path) as json_file:
             json_struct = json.load(json_file)
@@ -39,8 +41,10 @@ def load_json(json_directory_path: str) -> list:
                 keypoints = json_struct["people"][0]["pose_keypoints_2d"]
             except:
                 # TODO: log error
+                print("empty frame!", file=sys.stderr)
                 continue
             keypoint_dict = {}
+            keypoint_dict["ID"] = counter
             iterator = iter(keypoints)
             i = 0
             for x in iterator:
@@ -57,6 +61,10 @@ if __name__ == "__main__":
     # load json data from arg1 path
     directory = sys.argv[1]
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(load_json(directory))
+    data = load_json(directory)
+
+    pp.pprint(data)
+    
+
 
     
