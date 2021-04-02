aeedfc87886172fb283c428fd06d13c915bedd30
Radoslav Elias <radoslave0@gmail.com>
2021-04-02 16:51:31 +0200

    gui drag and drop option

--- src/gui/run.py
+++ src/gui/run.py
@@ -4,13 +4,28 @@ from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
 qtCreatorFile = "run_analysis.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
-
+    
 
 class AppWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls:
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        if event.mimeData().hasUrls:
+            self.video_url = event.mimeData().text()
+        else:
+            event.ignore()
+
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
+        self.video_url = None
         self.setupUi(self)
+
 
 
 if __name__ == "__main__":
